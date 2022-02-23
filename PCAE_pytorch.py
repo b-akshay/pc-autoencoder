9d2f7c2c0ee1940e51f8bc3f1a6d47247497a276
Akshay Balsubramani <abalsubr@stanford.edu>
2022-02-23 15:50:15 -0800

    make notebook to run data (TBD: check in with a vanilla MLP autoencoder model) 2/

--- PCAE_pytorch.py
+++ PCAE_pytorch.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import numpy as np
 
 from math import pi
@@ -6,9 +7,9 @@ from scipy.special import logsumexp
 
 # Placeholder: add an MLP autoencoder
 
+# Otherwise follow https://tomekkorbak.com/2021/02/09/em-for-gaussian-mixtures-using-einsum/
 
-
-class PCAutoencoder(torch.nn.Module):
+class PCAutoencoder(nn.Module):
     def __init__(self, n_hidden):
         super(PCAutoencoder, self).__init__()
 
